9591ab2ef5055dc946073b01e858e92e1b2cd440
ahmadag <ah.agbaria.99@gmail.com>
2021-03-21 12:21:28 +0200

    change TestThreadCheckArray

--- exc2/src/exc2/TestThreadCheckArray.java
+++ exc2/src/exc2/TestThreadCheckArray.java
@@ -1,4 +1,5 @@
 package exc2;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
@@ -7,11 +8,11 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<Integer>(num);
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+				array.set(index, input.nextInt());
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
